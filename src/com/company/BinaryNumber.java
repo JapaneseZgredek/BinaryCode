4a7a1f00122c6dee063e159a97ed34d9d767e8b8
Fabian Zbrański <fabian.zbranski@gmail.com>
2021-10-24 18:14:26 +0200

    Add Constructor, Getter and Setter

---
+++ src/com/company/BinaryNumber.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class BinaryNumber {
+    BinaryNumber(int number){
+        this.number = number;
+    }
+    private int number;
+    void setNumber(int number){
+        this.number = number;
+    }
+    int getNumber(){
+        return this.number;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        BinaryNumber mainNumber = new BinaryNumber(n1);
+        System.out.println(mainNumber.getNumber());
+    }
+}
